ce5688130050c7f4805bdcd20f65fa133e412ffe
moousumii <19165083+moousumii@users.noreply.github.com>
2023-03-24 16:21:13 -0400

    made common function

--- js/script.js
+++ js/script.js
@@ -1,136 +1,88 @@
+/* Common Functions */
+
 let selectedStudents = [];
 
-function selectStudent(studentName){
-    let studentList = document.getElementById("studentsList");
+function selectStudent(elementID, selectStudentName){
+    let btn = document.getElementById(elementID);
+    let studentName = document.getElementById(selectStudentName).innerText;
+    let studentList = document.getElementById('studentsList');
     let list = document.createElement("li");
     list.innerHTML = `<span class="text-gray-900 font-normal">${studentName}</span>`;
     if(selectedStudents.length !== 5) {
         selectedStudents.push(list);
+        btn.disabled = true;
+        btn.style.backgroundColor = "rgb(59 130 246)"
     }
+    else{
+        alert("You can select upto 5 students only!");
+    }
+
     for(let i = 0; i < 5; i++){
         studentList.appendChild(selectedStudents[i]);
     }
 }
 
+function getInputValue(inputID){
+    const inputElement = document.getElementById(inputID);
+    const inputElementString = inputElement.value;
+    const inputValue = parseFloat(inputElementString);
 
-function yusufRichard(){
-    let btn = document.getElementById("yusufRichard");
-    if(selectedStudents.length >= 5) {
-    alert("You can select upto 5 students only!");
+    if(isNaN(inputValue)) {
+        alert("Please Enter Number Value!");
+        inputElementString = '';
+        return;
     }
-    else{
-        btn.disabled = true;
-        btn.style.backgroundColor = "rgb(59 130 246)"
-    }
-    selectStudent("Yusuf Richard");
+    return inputValue;
 }
 
-function tyreseMata(){
-    let btn = document.getElementById("tyreseMata");
-    if(selectedStudents.length >= 5) {
-    alert("You can select upto 5 students only!");
-    }
-    else{
-        btn.disabled = true;
-        btn.style.backgroundColor = "rgb(59 130 246)"
-    }
-    selectStudent("Tyrese Mata");
-}
 
-function shaylaLivingston(){
-    let btn = document.getElementById("shaylaLivingston");
-    if(selectedStudents.length >= 5) {
-    alert("You can select upto 5 students only!");
-    }
-    else{
-        btn.disabled = true;
-        btn.style.backgroundColor = "rgb(59 130 246)"
-    }
-    selectStudent("Shayla Livingston");
-}
+/* Student Selection */
 
-function gavynMclean(){
-    let btn = document.getElementById("gavynMclean");
-    if(selectedStudents.length >= 5) {
-    alert("You can select upto 5 students only!");
-    }
-    else{
-        btn.disabled = true;
-        btn.style.backgroundColor = "rgb(59 130 246)"
-    }
-    selectStudent("Gavyn Mclean");
-}
+document.getElementById('yusufRichard').addEventListener('click', function(){
+    selectStudent('yusufRichard', 'nameYusuf');
+})
+document.getElementById('tyreseMata').addEventListener('click', function(){
+    selectStudent('tyreseMata', 'nameTyrese');
+})
+document.getElementById('shaylaLivingston').addEventListener('click', function(){
+    selectStudent('shaylaLivingston', 'nameShayla');
+})
+document.getElementById('gavynMclean').addEventListener('click', function(){
+    selectStudent('gavynMclean', 'nameGavyn');
+})
+document.getElementById('linaPage').addEventListener('click', function(){
+    selectStudent('linaPage', 'nameLina');
+})
+document.getElementById('cashPitts').addEventListener('click', function(){
+    selectStudent('cashPitts', 'nameCash');
+})
+document.getElementById('dominickBall').addEventListener('click', function(){
+    selectStudent('dominickBall', 'nameDominick');
+})
+document.getElementById('samaraMata').addEventListener('click', function(){
+    selectStudent('samaraMata', 'nameSamara');
+})
+document.getElementById('evaAbbott').addEventListener('click', function(){
+    selectStudent('evaAbbott', 'nameEva');
+})
 
-function linaPage(){
-    let btn = document.getElementById("linaPage");
-    if(selectedStudents.length >= 5) {
-    alert("You can select upto 5 students only!");
-    }
-    else{
-        btn.disabled = true;
-        btn.style.backgroundColor = "rgb(59 130 246)"
-    }
-    selectStudent("Lina Page");
-}
 
-function cashPitts(){
-    let btn = document.getElementById("cashPitts");
-    if(selectedStudents.length >= 5) {
-    alert("You can select upto 5 students only!");
-    }
-    else{
-        btn.disabled = true;
-        btn.style.backgroundColor = "rgb(59 130 246)"
-    }
-    selectStudent("Cash Pitts");
-}
+/* Calculation */
 
-function dominickBall(){
-    let btn = document.getElementById("dominickBall");
-    if(selectedStudents.length >= 5) {
-    alert("You can select upto 5 students only!");
-    }
-    else{
-        btn.disabled = true;
-        btn.style.backgroundColor = "rgb(59 130 246)"
-    }
-    selectStudent("Dominick Ball");
-}
-
-function samaraMata(){
-    let btn = document.getElementById("samaraMata");
-    if(selectedStudents.length >= 5) {
-    alert("You can select upto 5 students only!");
-    }
-    else{
-        btn.disabled = true;
-        btn.style.backgroundColor = "rgb(59 130 246)"
-    }
-    selectStudent("Samara Mata");
-}
-
-function evaAbbott(){
-    let btn = document.getElementById("evaAbbott");
-    if(selectedStudents.length >= 5) {
-    alert("You can select upto 5 students only!");
-    }
-    else{
-        btn.disabled = true;
-        btn.style.backgroundColor = "rgb(59 130 246)"
-    }
-    selectStudent("Eva Abbott");
-}
-
-function calcvoucher(){
-    const perVoucher = document.getElementById("giftVoucher").value;
-    const totalVoucherCost = document.getElementById("voucherCost");
-    totalVoucherCost.innerText = selectedStudents.length * parseFloat(perVoucher);
-}
+document.getElementById('calcCoupon').addEventListener('click', function(){
+    const perCoupon = getInputValue('giftCoupon');
+    const totalCouponCost = document.getElementById('couponCost');
+    totalCouponCost.innerText = selectedStudents.length * perCoupon;
+})
 
 function calcTotal(){
-    const perVoucher = document.getElementById("giftVoucher").value;
-    const travelTicketCost = document.getElementById("travelTicket").value;
-    const airbnbCost = document.getElementById("airbnb").value;
-    const totalCost = document.getElementById("totalCost");
-    totalCost.innerText = selectedStudents.length * parseFloat(perVoucher) + parseFloat(travelTicketCost) + parseFloat(airbnbCost);
+    const totalCouponCost = document.getElementById('couponCost');
+    const totalCouponCostString = totalCouponCost.innerText;
+    const couponCost = parseFloat(totalCouponCostString);
+
+    const tourGuideCost = getInputValue('tourGuide');
+    const airbnbCost = getInputValue('airbnb');
+
+    const totalCost = document.getElementById('totalCost');
+    totalCost.innerText = couponCost + tourGuideCost + airbnbCost;
 }
